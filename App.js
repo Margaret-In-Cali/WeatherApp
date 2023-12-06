a9c18bf29a069058529537c0360f216e47d30a2c
margaret-in-cali <margaret.wood.teague@gmail.com>
2023-12-05 16:03:22 -0800

    Latest changes, including the beginnings of styling

--- App.js
+++ App.js
@@ -1,22 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
-  const message = "Hello, World!";
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>{message}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8 </Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+        </View>
+        <View style={styles.bodyWrapper}>
+          <Text style={styles.description}>It's sunny</Text>
+          <Text style={styles.message}>It's perfect t-shirt weather</Text>
+        </View>
+    </SafeAreaView>
+  )
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    height: 200,
-    width: 200,
-    backgroundColor: 'orange',
-    paddingLeft: 80,
-    paddingTop: 80,
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 30,
   },
+
+  wrapper: {
+    backgroundColor: 'pink',
+    flex: 1,
+  },
+
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+
+  feels: {
+    fontSize: 30,
+    color: 'black',
+  },
+
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+
+  description: {
+    fontSize: 48,
+  },
+
+  message: {
+    fontSize: 30,
+  }
+
 });
